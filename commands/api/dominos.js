f6a6e5abd81e79fb6616543a54181f19f63f4323
Glazelf <gideon.steenbergen@gmail.com>
2021-03-02 15:13:09 +0100

    add handling

--- commands/api/dominos.js
+++ commands/api/dominos.js
@@ -3,22 +3,34 @@ module.exports.run = async (client, message, args) => {
         let pizzapi = require('domino');
         const { DMAllowed } = require('../../database/dbObjects');
 
-        let DMAllowedUser = await DMAllowed.findOne({ where: { user_id: message.author.id } });
+        var DMAllowedUser = await DMAllowed.findOne({ where: { user_id: message.author.id } });
         if (!DMAllowedUser) await DMAllowed.upsert({ user_id: message.author.id });
-        DMAllowedUser = DMAllowed.findOne({ where: { user_id: message.author.id } });
 
         message.channel.send(`> Okay, I will try to DM you now to take care of your order, ${message.author}.`);
-        message.author.send("epic dm momento");
+        let initDM = await message.author.send(`> Please enter your postal code, ${message.author}.`);
+        let filter = collected => collected.author.id === message.author.id;
 
-        // pizzapi.Util.findNearbyStores(
-        //     '63102',
-        //     'Delivery',
-        //     function (storeData) {
-        //         console.log(storeData);
-        //     }
-        // );
+        await initDM.channel.awaitMessages(filter, { max: 1, time: 10000, errors: ['time'] })
+            .then(async (collected) => {
 
-        await channelID.destroy();
+                console.log(collected)
+                pizzapi.Util.findNearbyStores(
+                    '63102',
+                    'Delivery',
+                    function (storeData) {
+                        console.log(storeData);
+                    }
+                );
+
+            })
+            .catch(async (error) => {
+                console.log(error)
+                await DMAllowedUser.destroy();
+                return message.author.dmChannel.send(`> An error occurred or your order timed out, please try again, ${message.author}.`);
+            });
+
+        DMAllowedUser = await DMAllowed.findOne({ where: { user_id: message.author.id } });
+        if (DMAllowedUser) await DMAllowedUser.destroy();
         return message.channel.send(`> Successfully completed your order, ${message.author}. I will DM you updates:tm:.`);
 
     } catch (e) {
